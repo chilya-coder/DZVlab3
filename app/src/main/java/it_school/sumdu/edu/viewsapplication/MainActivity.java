802873672d2d44b7d6aba29eb20ae1097df4db51
chilya-coder <yuliya.chimyrys@gmail.com>
2020-04-28 14:16:21 +0300

    Initial commit

---
+++ app/src/main/java/it_school/sumdu/edu/viewsapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package it_school.sumdu.edu.viewsapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+    int clicks = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View topButtonPanel = findViewById(R.id.top_button_panel);
+        View bottomButtonPanel = findViewById(R.id.bottom_button_panel);
+        final TextView clicksText = findViewById(R.id.clicksText);
+        Button topButton = topButtonPanel.findViewById(R.id.clickBtn);
+        Button bottomButton = bottomButtonPanel.findViewById(R.id.clickBtn);
+        topButton.setText("+");
+        bottomButton.setText("-");
+        topButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicks++;
+                clicksText.setText(clicks + " Clicks");
+            }
+        });
+        bottomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicks--;
+                clicksText.setText(clicks + " Clicks");
+            }
+        });
+    }
+}
